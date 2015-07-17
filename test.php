ac3a1f7e11322939f74253d7df1faba80465f5bf
rasclatt <rasclatt@me.com>
2015-07-16 20:09:48 -0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,11 @@
+<style>
+body	{
+	background-color: #D8D2C6;
+}
+p	{
+	line-height: 26px;
+}
+</style>
+<h3>This is just a test.</h3>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam luctus arcu sed enim tempus vestibulum. Cras non neque sapien. Maecenas sed tempor nibh. In nibh nibh, facilisis eu luctus et, faucibus sed purus. Quisque sed ultrices lorem. Nulla accumsan ex commodo, porta purus non, vulputate magna. Pellentesque quis diam ac diam sagittis egestas eu auctor odio. Aenean vel metus elementum, vehicula lacus quis, suscipit urna. Nunc fringilla rhoncus quam, sed interdum neque fermentum sed. Nam pulvinar sed lorem sit amet mattis.</p>
+<p>Maecenas cursus vestibulum nibh vitae consequat. Mauris mi nunc, laoreet id arcu ac, pretium dictum eros. Vestibulum viverra vestibulum felis, in mattis tellus ullamcorper sit amet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer egestas ex sed nunc semper, quis consequat ligula laoreet. Phasellus vel massa non metus rhoncus luctus a in est. Integer interdum sagittis ligula a aliquet. Morbi vulputate ligula vel dictum tristique. Curabitur eget neque eleifend, rutrum massa id, blandit nunc. Suspendisse egestas dui nec orci tincidunt fermentum. Donec quis purus elit.</p>
