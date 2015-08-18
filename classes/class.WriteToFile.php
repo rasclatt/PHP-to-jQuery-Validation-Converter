df5bb5fb130f5991902545ae617aa10efe05f9d1
rasclatt <rasclatt@me.com>
2015-08-18 11:24:46 -0700

    Update class.WriteToFile.php

--- classes/class.WriteToFile.php
+++ classes/class.WriteToFile.php
@@ -10,8 +10,7 @@
 			
 			public	function AddInput($payload =  array())
 				{
-					if(isset($payload) && !empty($payload)) {
-							AutoloadFunction('check_empty');
+					if(!empty($payload)) {
 							if(!empty($payload['content']) && !empty($payload['save_to'])) {
 									$this->data['content'][]	=	$payload['content'];
 									$this->data['save_to'][]	=	$payload['save_to'];
@@ -25,7 +24,6 @@
 			public	function SaveDocument()
 				{
 					if(isset($this->data) && !empty($this->data)) {
-							AutoloadFunction('write_file');
 							foreach($this->data['content'] as $key => $value) {
 									write_file(array("content"=>$value,"type"=>$this->data['type'][$key],"save_to"=>$this->data['save_to'][$key]));
 								}
