1fc12ac12dbee7b0950799b9ad49b09a5bce2ff8
rasclatt <rasclatt@me.com>
2015-08-18 11:44:48 -0700

    Update function.write_file.php

--- functions/function.write_file.php
+++ functions/function.write_file.php
@@ -2,48 +2,36 @@
 /*
  * @type (array)
  * @param ['content'] => The content to write to disk => DEFAULT => false
- * @param ['save_to'] => The filename including directory => DEFAULT => false
+ * @param ['save_name'] => The filename => DEFAULT => false
+ * @param ['save_to'] => The directory to save the file in => DEFAULT => false
  * @param ['type'] => This is the way the file is to be saved. DEFAULT => 'a'
 */
 	function write_file($settings = false)
 		{
-			$settings['content']	=	(!empty($settings['content']))? $settings['content']: false;
-			$settings['save_to']	=	(!empty($settings['save_to']))? $settings['save_to']: false;
-			$settings['type']	=	(!empty($settings['type']))? $settings['type']: 'a';
-			
-			if($settings['save_to'] == false)
-				return;
-				
-			/***********************************/
-			/***** BREAK OUT THE DIRECTORY *****/
-			/***********************************/
-			
-			$dir_exp	=	explode("/",$settings['save_to']);
-			$dir_exp	=	array_filter($dir_exp);
-			array_pop($dir_exp);
-			$dir		=	implode("/",$dir_exp);
-			
-			/***********************************/
-			/***********************************/
-			
+			$content	=	(!empty($settings['content']))? $settings['content']: false;
+			$dir		=	(!empty($settings['save_dir']))? $settings['save_dir']: false;
+			$filename	=	(!empty($settings['save_name']))? $settings['save_name']: false;
+			$type		=	(!empty($settings['type']))? $settings['type']: 'a';
 			// Set default write to false
 			$write		=	false;
+			
+			if(!$dir || !$filename)
+				return;
+			
 			// Create folder if not exists
-			if(!is_dir($dir)) {
-					if(mkdir($dir,0755,true))
-						$write	=	true;
-				}
+			if(!is_dir($dir))
+				$write	=	(mkdir($dir,0755,true))? true : false;
 			else
 				$write	=	true;
 				
 			// If all is good, write file
-			if($write != true)
+			if(!$write)
 				return;
-			
-			$fh	=	fopen($settings['save_to'], $settings['type']);
-			fwrite($fh, $settings['content']);
+			$file	=	str_replace("//","/",$dir.$filename);
+			$fh		=	fopen($file, $type);
+			fwrite($fh, $content);
 			fclose($fh);
 			
-			return (is_file($settings['save_to']))? true:false;
+			return (is_file($file))? true:false;
 		}
 ?>
