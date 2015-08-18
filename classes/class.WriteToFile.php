59d15e39a2939f74dfc6a1afef892a2afc7e4f25
rasclatt <rasclatt@me.com>
2015-08-18 11:21:17 -0700

    Create class.WriteToFile.php

---
+++ classes/class.WriteToFile.php
@@ -0,0 +1,37 @@
+<?php
+	class WriteToFile
+		{
+			protected	$data;
+			
+			public	function __construct()
+				{
+					
+				}
+			
+			public	function AddInput($payload =  array())
+				{
+					if(isset($payload) && !empty($payload)) {
+							AutoloadFunction('check_empty');
+							if(!empty($payload['content']) && !empty($payload['save_to'])) {
+									$this->data['content'][]	=	$payload['content'];
+									$this->data['save_to'][]	=	$payload['save_to'];
+									$this->data['type'][]		=	(!empty($payload['type']))? $payload['type']:"a";
+								}
+						}
+						
+					return $this;
+				}
+			
+			public	function SaveDocument()
+				{
+					if(isset($this->data) && !empty($this->data)) {
+							AutoloadFunction('write_file');
+							foreach($this->data['content'] as $key => $value) {
+									write_file(array("content"=>$value,"type"=>$this->data['type'][$key],"save_to"=>$this->data['save_to'][$key]));
+								}
+						}
+						
+					return $this;
+				}
+		}
+?>
